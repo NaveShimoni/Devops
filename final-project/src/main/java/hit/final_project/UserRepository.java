254fd0889efad8edf7dc6f6368c4b31185e289c2
Amit Shiber <amitshiber2001@gmail.com>
2024-08-07 18:42:24 +0300

    without user files

--- final-project/src/main/java/hit/final_project/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package hit.final_project;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}
